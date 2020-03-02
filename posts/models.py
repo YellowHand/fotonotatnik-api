d9a0b6a5d621d81c1c6832b946553e962ac7f395
Paweł Borkowski <ypawelborkowski@gmai.com>
2020-03-02 13:10:11 +0100

    post endpoint crud

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
